7bb79db81ad5c94b42875bf9974cd86f7222789c
Ofure004 <ofure.ehiremhen@stu.cu.edu.ng>
2023-09-17 00:51:49 +0100

    caved and used a postgresql instead, sigh

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     implementation("jakarta.validation:jakarta.validation-api:3.0.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("com.mysql:mysql-connector-j")
+    implementation("org.postgresql:postgresql:42.6.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
