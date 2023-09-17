5144ba57658d08665e226735d6a573b36045cae3
Ofure004 <ofure.ehiremhen@stu.cu.edu.ng>
2023-09-17 12:26:35 +0100

    regex validation for my age string

--- src/main/kotlin/com/example/hng_stage_two/model/entity/Person.kt
+++ src/main/kotlin/com/example/hng_stage_two/model/entity/Person.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.validation.constraints.Pattern
 
 
 @Entity
@@ -12,6 +13,7 @@ data class Person(
     var id: Int,
     var firstName: String? = null,
     var lastName: String? = null,
+    @field:Pattern(regexp = "^\\d+\$", message = "Age must only contain numbers")
     var age: String? = null,
     var track: String? = null
 )
